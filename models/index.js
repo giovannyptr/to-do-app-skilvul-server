1e4f60635b6b1be698c2d2ece91f66021e6bf8e6
Giovanny Putri Andini <86881191+giovannyptr@users.noreply.github.com>
2023-10-02 12:36:26 +0800

    ols

--- models/index.js
+++ models/index.js
@@ -1,33 +1,20 @@
-'use strict';
-
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
+const config = require('../config/dbConfig'); // Assuming dbConfig.js is in the root directory
+
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/dbConfig.js')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = config;  // Use the sequelize instance from dbConfig.js
 
 fs
   .readdirSync(__dirname)
   .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
